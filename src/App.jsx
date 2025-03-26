1e17f4567ad2a74f71491e678acb87a12f2f7a43
Sujit <pandasujit687@gmail.com>
2025-03-27 00:16:42 +0530

    new component added for main content

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css'
 import Navbar from './components/Navbar.jsx'
+import Hero from './components/Hero.jsx'
 
 function App() {
   return (
  <div>
   <Navbar />
+  <Hero/>
  </div>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
 function Hero() {
   return (
-    <div>Hero</div>
-  )
+    <main className="hero">
+      <div className="hero-content">
+        <h1>RUN IT UP!!!</h1>
+        <p>
+          "Nike shoes combine cutting-edge technology, style, and comfort for
+          athletes and everyday wear. Designed for performance and durability,
+          they offer superior cushioning and support. Elevate your game with
+          iconic designs and innovative features!"
+        </p>
+        <div className="hero-button">
+          <button>Shop Now</button>
+          <button>Category</button>
+        </div>
+      </div>
+      <div className="hero-image"></div>
+    </main>
+  );
 }
 
-export default Hero
+export default Hero;
