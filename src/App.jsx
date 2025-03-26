fd7577cde1d1bdce35d545cc7b0fd3ab62febe55
Sujit <pandasujit687@gmail.com>
2025-03-26 23:39:28 +0530

    New app configuration

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+ <div>
+  <nav>
+    <div className="logo">
+      <img src="/images/brand_logo.png" alt="" />
+    </div>
+    <ul>
+      <li href="#">Menu</li>
+      <li href="#">Location</li>
+      <li href="#">About</li>
+      <li href="#">Contact</li>
+    </ul>
+
+    <button>login</button>
+  </nav>
+ </div>
+  )
+}
+
+export default App
