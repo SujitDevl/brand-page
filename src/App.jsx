8e11998185c329c5162b55caa2c010be3c58825e
Sujit <pandasujit687@gmail.com>
2025-03-27 00:06:48 +0530

    setting up components for management

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,11 @@
 import React from 'react'
 import './App.css'
+import Navbar from './components/Navbar.jsx'
 
 function App() {
   return (
  <div>
-  <nav>
-    <div className="logo">
-      <img src="/images/brand_logo.png" alt="" />
-    </div>
-    <ul>
-      <li href="#">Menu</li>
-      <li href="#">Location</li>
-      <li href="#">About</li>
-      <li href="#">Contact</li>
-    </ul>
-
-    <button>login</button>
-  </nav>
+  <Navbar />
  </div>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div>
+      <nav>
+        <div className="logo">
+          <img src="/images/brand_logo.png" alt="" />
+        </div>
+        <ul>
+          <li href="#">Menu</li>
+          <li href="#">Location</li>
+          <li href="#">About</li>
+          <li href="#">Contact</li>
+        </ul>
+
+        <button>login</button>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
