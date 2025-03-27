d5ab8231b1c8c3ec0eb9cb9c316dd60317cb51c2
Sujit <pandasujit687@gmail.com>
2025-03-27 23:33:51 +0530

    description changes in p tag

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,12 +4,11 @@ function Hero() {
   return (
     <main className="hero">
       <div className="hero-content">
-        <h1>RUN IT UP!!!</h1>
+        <h1>RUN IT UP...</h1>
         <p>
           "Nike shoes combine cutting-edge technology, style, and comfort for
           athletes and everyday wear. Designed for performance and durability,
-          they offer superior cushioning and support. Elevate your game with
-          iconic designs and innovative features!"
+          they offer superior cushioning and support.
         </p>
         <div className="hero-button">
           <button>Shop Now</button>
